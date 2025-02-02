d8fbac1a4fd42d03949f55d83de3064cfbdd0846
Jonathanpv99 <jonyvegapjv@gmail.com>
2025-02-02 16:07:26 -0600

    feat: add navigation && theme color

--- app/src/main/java/com/example/cronoapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/cronoapp/ui/theme/Color.kt
@@ -1,7 +1,9 @@
 package com.example.cronoapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
+val Principal = Color(color = 0xFFF4511E)
 
+//default
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
